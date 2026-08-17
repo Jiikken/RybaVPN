3eeaa6e2ac1a3124c2793cb777ad9bc810a17b62
Jiikken <Jiken.rf@yandex.ru>
2026-08-17 15:21:56 +0700

    add custom colors

---
+++ lib/constants/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+Color blue = const Color(0xff82B3BF);
+
+Color greyText = const Color(0xff454545);
+Color greyBackground = const Color(0xff1A1A1A);
+Color greyBackgroundSection = const Color(0xff121212);
+Color greyBackgroundButton = const Color(0xff1D1D1D);
+Color greyBackgroundStartButton = const Color(0xff171717);
+Color greyBackgroundButtonTopSection = const Color(0xff1E1E1E);
+
+Color white = const Color(0xffFFFFFF);
