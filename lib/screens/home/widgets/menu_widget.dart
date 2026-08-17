2c21621988e5ec54f84c5106d2dce855b91d8582
Jiikken <Jiken.rf@yandex.ru>
2026-08-17 15:22:19 +0700

    add menu

--- lib/screens/home/widgets/menu_widget.dart
+++ lib/screens/home/widgets/menu_widget.dart
@@ -9,8 +9,40 @@ class MenuWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 200,
-      height: 58,
       color: const Color(0xff121212),
+      padding: const EdgeInsets.all(4),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.grey,
+              height: 50,
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                "Home",
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 50,
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                "Info",
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 50,
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                "Settings",
+              ),
+            )
+          )
+        ],
+      ),
     );
   }
 }
