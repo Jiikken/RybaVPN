c9e8b10a272ec4bacaaa4c6a0ab5e852251f1fd9
Jiikken <Jiken.rf@yandex.ru>
2026-08-17 16:27:02 +0700

    create circles

--- lib/screens/home/widgets/top_widget.dart
+++ lib/screens/home/widgets/top_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trying_flutter/constants/constants.dart';
 
 class TopWidget extends StatelessWidget {
   const TopWidget({
@@ -13,31 +14,43 @@ class TopWidget extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
+            decoration: BoxDecoration(
+              color: greyBackgroundButtonTopSection.withValues(alpha: 0.5),
+              shape: BoxShape.circle,
+            ),
             width: 38,
             height: 38,
-            color: const Color(0xff121212),
             margin: EdgeInsets.only(left: 14),
           ),
           Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff121212),
+              borderRadius: BorderRadius.circular(15)
+            ),
             width: 195,
             height: 56,
-            color: const Color(0xff121212),
             margin: EdgeInsets.only(left: 34, right: 34),
             child: Row(
               children: [
                 Expanded(
                   child: Text(
-                    "RybaBot",
+                    "RybaVPN",
                     textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 32
+                    ),
                   ),
                 ),
               ],
             ),
           ),
           Container(
+            decoration: BoxDecoration(
+              color: greyBackgroundButtonTopSection.withValues(alpha: 0.5),
+              shape: BoxShape.circle,
+            ),
             width: 38,
             height: 38,
-            color: const Color(0xff121212),
             margin: EdgeInsets.only(right: 14),
           ),
         ],
